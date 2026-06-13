28afc34ce236355f64eab307dd2f6505f01d1d2e
svgh1465 <svgh1465@gmail.com>
2026-06-12 20:48:55 -0600

    Initial commit

--- script.R
+++ script.R
@@ -2,3 +2,4 @@ library(readr)
 Data_Science_Fields_Salary_Categorization <- read_csv("Data_Science_Fields_Salary_Categorization.csv")
 View(Data_Science_Fields_Salary_Categorization)
 
+#Prueba con la base de datos.
